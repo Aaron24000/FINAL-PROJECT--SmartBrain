4b7b47409774603b676389a02a6974c2d560685e
Aaron24000 <aaron.noble@outlook.com>
2020-12-14 21:59:46 -0500

    Addinf CSS components and setting up front-end

---
+++ smart-brain/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        {/*
+                <ImageLinkForm />
+                <FaceRecognition />*/}
+      </div>
+    );
+  }
+
+}
+
+export default App;
