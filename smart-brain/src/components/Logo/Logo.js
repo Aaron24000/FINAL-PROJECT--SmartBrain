025944ca13f2c6660c76859510653514d3c8926f
Aaron24000 <aaron.noble@outlook.com>
2020-12-15 06:53:18 -0500

    Adding brain to css

--- smart-brain/src/components/Logo/Logo.js
+++ smart-brain/src/components/Logo/Logo.js
@@ -2,12 +2,16 @@ import React from 'react';
 import 'tachyons';
 import Tilt from 'react-tilt';
 import './Logo.css';
+import './brain.png';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max: 25 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner">HI</div>
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner">
+                <img alt='logo' src={brain}>
+                </img>
+                </div>
             </Tilt>
         </div>
     );
