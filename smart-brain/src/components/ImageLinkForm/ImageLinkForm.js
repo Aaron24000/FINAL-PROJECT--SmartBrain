e80aa2f592acb182dceaf6a87e5ef44ba0d68f09
Aaron24000 <aaron.noble@outlook.com>
2020-12-15 07:54:57 -0500

    Added honeycomb (tachyons will be fixed later on!)

--- smart-brain/src/components/ImageLinkForm/ImageLinkForm.js
+++ smart-brain/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import 'tachyons';
+import React from "react";
+import "tachyons";
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
-    return (
-      <div>
-      <p className= 'f3'>
-          {'This Magic Brain will detect faces in your pictures. Give it a try.'}
+  return (
+    <div>
+      <p className="f3">
+        {"This Magic Brain will detect faces in your pictures. Give it a try."}
       </p>
-      <div>
+      <div className='center'>
+        <div className='form center pa4 br3 shadow-5'>
           <input ClassName='f4 pa2 w-70 center' type='tex' />
-          <button ClassName='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+          <button ClassName='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>
+            Detect
+          </button>
+        </div>
       </div>
-      </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default ImageLinkForm;
