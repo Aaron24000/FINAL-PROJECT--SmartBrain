8476bf530b06932289c0978214632e051c9032f5
Aaron24000 <aaron.noble@outlook.com>
2020-12-15 06:56:07 -0500

    brain save

--- smart-brain/src/components/Logo/Logo.js
+++ smart-brain/src/components/Logo/Logo.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import 'tachyons';
-import Tilt from 'react-tilt';
-import './Logo.css';
-import './brain.png';
+import React from "react";
+import "tachyons";
+import Tilt from "react-tilt";
+import "./Logo.css";
+import "./brain.png";
 
 const Logo = () => {
-    return (
-        <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner">
-                <img alt='logo' src={brain}>
-                </img>
-                </div>
-            </Tilt>
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa3">
+          <img style={{ paddingTop: "5px" }} alt="logo" src="brain.png"></img>
         </div>
-    );
-}
+      </Tilt>
+    </div>
+  );
+};
 
 export default Logo;
