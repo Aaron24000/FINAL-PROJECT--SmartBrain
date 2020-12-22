2b046c61f81cdd768393d3acbcc2033c6270ab87
Aaron24000 <aaron.noble@outlook.com>
2020-12-22 18:15:41 -0500

    rEMOVED PORT 3000 FROM FETCH TO FIX ERROR

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch("https://limitless-badlands-77586.herokuapp.com:3000/imageurl", {
+    fetch("https://limitless-badlands-77586.herokuapp.com/imageurl", {
       method: "post",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
@@ -88,7 +88,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("https://limitless-badlands-77586.herokuapp.com:3000/image", {
+          fetch("https://limitless-badlands-77586.herokuapp.com/image", {
             method: "put",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
